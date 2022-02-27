bb34c9ee7fe632b5138b83109b81b9cfbd3fa41e
ITMASTER\a.aresta <a.aresta@almaviva.it>
2022-02-27 12:03:09 +0100

    excluded junit jupiter

--- src/test/java/it/beergash/management/account/service/GetAccountServiceTest.java
+++ src/test/java/it/beergash/management/account/service/GetAccountServiceTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
